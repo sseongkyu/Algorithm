7c77c84721d370f2f01cb94ad5ab18662ac09598
sseongkyu <ssk9807@gmail.com>
2022-05-10 09:55:36 +0900

    FloydWarshall

--- ShortestPath/floyd-warshall.py
+++ ShortestPath/floyd-warshall.py
@@ -1,36 +1,38 @@
-INF = int(1e9)  # infinity value
+# 모든 노드에 모든 노드의 최소거리를 구하는 알고리즘
+# 점화식 D(ab) = min( D(ab), D(ak)+D(kb) )
 
-# input node, edge
+INF = int(1e9)
+
+# 노드 개수 및 간섭 개수 입력
 n = int(input())
 m = int(input())
-# make 2Dimension list, reset to infinity
-graph = [[INF] * (n+1) for _ in range(n+1)]
+# 2차원 리스트(그래프 표현)를 만들고, 무한으로 초기화
+graph = [[INF] * (n-1) for _ in range(n+1)]
 
-# The Cost of the diagonal element is reset to 0
+# 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
 for a in range(1, n+1):
     for b in range(1, n+1):
         if a == b:
             graph[a][b] = 0
 
-# Input the information of each edge and initializes
+# 각 간선 정보를 입력, 그 값으로 초기화
 for _ in range(m):
-    # Cost of going from A to B is C
+    # A에서 B로 가는 비용은 C라고 설정
     a, b, c = map(int, input().split())
     graph[a][b] = c
 
-# perform floyd warshall algorithm according to the recurrence relation
+# floyd warshall argorithm
 for k in range(1, n+1):
     for a in range(1, n+1):
         for b in range(1, n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
 
-# result
+# 수행된 결과를 출력
 for a in range(1, n+1):
     for b in range(1, n+1):
-        # value == INF
+        # 도달할 수 없는 경우, INF출력
         if graph[a][b] == INF:
-            print('INFINITY', end=' ')
-        # valid value
+            print("INFINITY", end=" ")
         else:
-            print(graph[a][b], end=' ')
+            print(graph[a][b], end=" ")
     print()
